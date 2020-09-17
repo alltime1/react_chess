37b7e8b6c8d940e52e403fb598179af2dd4cb501
alltime1 <2956489346@qq.com>
2020-09-17 12:33:23 +0800

    'chess'

---
+++ copy/src/view/chessboard.jsx
@@ -0,0 +1,15 @@
+class chessboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  
+      <div>
+        我来了
+      </div>
+    );
+  }
+}
+ 
+export default chessboard;
